0c40987f62b03fb1d4030bfc6692e87ca0ff9ec6
IzzySoprano <Zerezghi@live.co.uk>
2022-07-22 07:21:25 +0100

    Added topup + withdraw  + checkbalance functionv

--- src/hello_assets/src/index.js
+++ src/hello_assets/src/index.js
@@ -1,19 +1,41 @@
 import { hello } from "../../declarations/hello";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+window.addEventListener("load", async function() {
+  // console.log("Finished looading")
+  update();
+});
 
-  const name = document.getElementById("name").value.toString();
+document.querySelector("form").addEventListener("submit", async function(event) {
+  event.preventDefault();
+  // console.log("submitted");
+  
+  const button = event.target.querySelector("#submit-btn");
+  
+  const inputAmount = parseFloat(document.getElementById("input-amount").value);
+  const outputAmount = parseFloat(document.getElementById("withdrawal-amount").value);
 
   button.setAttribute("disabled", true);
+  
+  if (document.getElementById("input-amount").value.length !=0 ) {
+    await hello.topUp(inputAmount);
+  }
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await hello.greet(name);
+  if (document.getElementById("withdrawal-amount").value.length !=0){
+    await hello.withDraw(outputAmount);
+}
+
+  await hello.compound();
+
+  update()
+
+  document.getElementById("input-amount").value = "";
+  document.getElementById("withdrawal-amount").value = "";
 
   button.removeAttribute("disabled");
 
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
 });
+
+async function update() {
+  const currentAmount = await hello.checkBalance();
+  document.getElementById("value").innerText = Math.round(currentAmount * 100) / 100;
+};
